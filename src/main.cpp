b10f6de21cae8f6966f79073d6227d67432021da
darbata <batarina.darren@gmail.com>
2026-08-14 21:20:28 +1000

    built clock using linear transformation matrices

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
+#include <math.h>
+
 #include "Colour.h"
 #include "Canvas.h"
+#include "Matrix.h"
+#include "Tuple.h"
 
 int main() {
-    Canvas c = Canvas(40, 40);
-    printColour(c.at(20, 20));
-    c.write_pixel(20, 20, Colour{0.5, 1.0, 0.5});
-    printColour(c.at(20, 20));
+    int size = 240;
+    Colour white {255, 255, 255};
+    Canvas c = Canvas(size, size);
+
+    Tuple twelve {0, 80, 0, 1.0};
+    for (int hour = 0; hour < 12; ++hour) {
+        Tuple p = rotation_z(hour * M_PI / 6) * twelve;
+        int x = static_cast<int>(p.x + size / 2.0);
+        int y = static_cast<int>(size / 2.0 - p.y);
+        c.write_pixel(x, y, white);
+    }
+
+
     canvas_to_ppm(&c);
     return 0;
 }
